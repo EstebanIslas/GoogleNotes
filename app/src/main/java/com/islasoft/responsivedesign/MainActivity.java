1b212ddeff5fa4eeb2d306c60554e06731da371f
Esteban Islas <1717110261@utectulancingo.edu.mx>
2020-12-01 16:02:05 -0600

    Creacion de Login y Navigation con alineacion y uso de guias

---
+++ app/src/main/java/com/islasoft/responsivedesign/MainActivity.java
@@ -0,0 +1,37 @@
+package com.islasoft.responsivedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (btn_login.isPressed()){
+            toast("Welcome!!");
+            Intent notes = new Intent(MainActivity.this, NotasActivity.class);
+            startActivity(notes);
+        }
+    }
+
+    private void toast(String txt){
+        Toast.makeText(this, txt, Toast.LENGTH_SHORT).show();
+    }
+}
